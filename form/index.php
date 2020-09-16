b856ed3792f835bdb0f9a7999e3da2b96d1f91ac
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-09-16 16:05:18 +0100

    Fixes typos

--- form/index.php
+++ form/index.php
@@ -5,26 +5,26 @@
 ADD: <input type="number" name="add1" placeholder="Enter a number">
 +
 <input type="number" name="add2" placeholder="Enter a second number">
-<button type="submit">Add</button></br>
+<button type="submit">Add</button><br>
 </form>
 
 <form method="GET" action="division.php">
 DIVIDE: <input type="number" name="divide1" placeholder="Enter a number">
 /
 <input type="number" name="divide2" placeholder="Enter a second number">
-<button type="submit">Divide</button></br>
+<button type="submit">Divide</button><br>
 </form>
 
 <form method="GET" action="multiply.php">
 MULTIPLY: <input type="number" name="divide1" placeholder="Enter a number">
 *
 <input type="number" name="multiply1" placeholder="Enter a second number">
-<button type="submit">Multiply</button></br>
+<button type="submit">Multiply</button><br>
 </form>
 
 <form method="GET" action="subtract.php">
 SUBTRACT: <intput type="number" name="subtract1" placeholder="Enter a number">
 -
 <input type="number" name="subtract1" placeholder="Enter a second number">
-<button type="submit">Subtract</button></br>
+<button type="submit">Subtract</button><br>
 
