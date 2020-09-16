90d1695a7e1becc502c7513dd0c5febf61696a69
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-09-16 16:03:50 +0100

    Adds main page with forms

---
+++ form/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<body>
+<form method="GET" action="addition.php">
+ADD: <input type="number" name="add1" placeholder="Enter a number">
++
+<input type="number" name="add2" placeholder="Enter a second number">
+<button type="submit">Add</button></br>
+</form>
+
+<form method="GET" action="division.php">
+DIVIDE: <input type="number" name="divide1" placeholder="Enter a number">
+/
+<input type="number" name="divide2" placeholder="Enter a second number">
+<button type="submit">Divide</button></br>
+</form>
+
+<form method="GET" action="multiply.php">
+MULTIPLY: <input type="number" name="divide1" placeholder="Enter a number">
+*
+<input type="number" name="multiply1" placeholder="Enter a second number">
+<button type="submit">Multiply</button></br>
+</form>
+
+<form method="GET" action="subtract.php">
+SUBTRACT: <intput type="number" name="subtract1" placeholder="Enter a number">
+-
+<input type="number" name="subtract1" placeholder="Enter a second number">
+<button type="submit">Subtract</button></br>
+
